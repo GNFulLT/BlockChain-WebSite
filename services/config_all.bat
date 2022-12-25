2c1e0e39836aab32cf7416ebd0edb5830380d2dc
GnfFullt <73427021+GNFulLT@users.noreply.github.com>
2022-12-25 19:36:34 +0300

    Update config_all.bat

--- services/config_all.bat
+++ services/config_all.bat
@@ -12,15 +12,15 @@ echo Network is successfully created. Building services
 
 echo Taueureka Discovery Service building
 
-call ./build_taueureka.bat
+call build_taueureka.bat
 
 echo Taugateway Service building
 
-call ./build_taugateway.bat
+call build_taugateway.bat
 
 echo Database Service building
 
-call ./build_database_service.bat
+call build_database_service.bat
 
 echo Now you can start up_all.bat
 
